c6a2bf88dbdefb1e352ac1fa41903c72a28214fb
Kanishk Kakar <kanishk.kakar@gmail.com>
2021-05-14 16:48:43 +0000

    Replace TF functionality with FlatEarth's.

--- commands.go
+++ commands.go
@@ -4,8 +4,6 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/mitchellh/cli"
-
 	"github.com/hashicorp/go-plugin"
 	svchost "github.com/hashicorp/terraform-svchost"
 	"github.com/hashicorp/terraform-svchost/auth"
@@ -15,48 +13,17 @@ import (
 	"github.com/hashicorp/terraform/command/cliconfig"
 	"github.com/hashicorp/terraform/internal/getproviders"
 	pluginDiscovery "github.com/hashicorp/terraform/plugin/discovery"
+	"github.com/hashicorp/terraform/terraform"
 )
 
-// runningInAutomationEnvName gives the name of an environment variable that
-// can be set to any non-empty value in order to suppress certain messages
-// that assume that Terraform is being run from a command prompt.
-const runningInAutomationEnvName = "TF_IN_AUTOMATION"
-
-// Commands is the mapping of all the available Terraform commands.
-var Commands map[string]cli.CommandFactory
-
-// PrimaryCommands is an ordered sequence of the top-level commands (not
-// subcommands) that we emphasize at the top of our help output. This is
-// ordered so that we can show them in the typical workflow order, rather
-// than in alphabetical order. Anything not in this sequence or in the
-// HiddenCommands set appears under "all other commands".
-var PrimaryCommands []string
-
-// HiddenCommands is a set of top-level commands (not subcommands) that are
-// not advertised in the top-level help at all. This is typically because
-// they are either just stubs that return an error message about something
-// no longer being supported or backward-compatibility aliases for other
-// commands.
-//
-// No commands in the PrimaryCommands sequence should also appear in the
-// HiddenCommands set, because that would be rather silly.
-var HiddenCommands map[string]struct{}
-
-// Ui is the cli.Ui used for communicating to the outside world.
-var Ui cli.Ui
-
-func initCommands(
+func getCommands(
 	originalWorkingDir string,
 	config *cliconfig.Config,
 	services *disco.Disco,
 	providerSrc getproviders.Source,
 	providerDevOverrides map[addrs.Provider]getproviders.PackageLocalDir,
 	unmanagedProviders map[addrs.Provider]*plugin.ReattachConfig,
-) {
-	var inAutomation bool
-	if v := os.Getenv(runningInAutomationEnvName); v != "" {
-		inAutomation = true
-	}
+) map[string]FlatEarthCommand {
 
 	for userHost, hostConfig := range config.Hosts {
 		host, err := svchost.ForComparison(userHost)
@@ -82,7 +49,7 @@ func initCommands(
 
 		Services: services,
 
-		RunningInAutomation: inAutomation,
+		RunningInAutomation: false,
 		CLIConfigDir:        configDir,
 		PluginCacheDir:      config.PluginCacheDir,
 		OverrideDataDir:     dataDir,
@@ -100,304 +67,12 @@ func initCommands(
 	// add, remove or reclassify commands then consider updating
 	// that to match.
 
-	Commands = map[string]cli.CommandFactory{
-		"apply": func() (cli.Command, error) {
-			return &command.ApplyCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"console": func() (cli.Command, error) {
-			return &command.ConsoleCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"destroy": func() (cli.Command, error) {
-			return &command.ApplyCommand{
-				Meta:    meta,
-				Destroy: true,
-			}, nil
-		},
-
-		"env": func() (cli.Command, error) {
-			return &command.WorkspaceCommand{
-				Meta:       meta,
-				LegacyName: true,
-			}, nil
-		},
-
-		"env list": func() (cli.Command, error) {
-			return &command.WorkspaceListCommand{
-				Meta:       meta,
-				LegacyName: true,
-			}, nil
-		},
-
-		"env select": func() (cli.Command, error) {
-			return &command.WorkspaceSelectCommand{
-				Meta:       meta,
-				LegacyName: true,
-			}, nil
-		},
-
-		"env new": func() (cli.Command, error) {
-			return &command.WorkspaceNewCommand{
-				Meta:       meta,
-				LegacyName: true,
-			}, nil
-		},
-
-		"env delete": func() (cli.Command, error) {
-			return &command.WorkspaceDeleteCommand{
-				Meta:       meta,
-				LegacyName: true,
-			}, nil
-		},
-
-		"fmt": func() (cli.Command, error) {
-			return &command.FmtCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"get": func() (cli.Command, error) {
-			return &command.GetCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"graph": func() (cli.Command, error) {
-			return &command.GraphCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"import": func() (cli.Command, error) {
-			return &command.ImportCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"init": func() (cli.Command, error) {
-			return &command.InitCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"login": func() (cli.Command, error) {
-			return &command.LoginCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"logout": func() (cli.Command, error) {
-			return &command.LogoutCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"output": func() (cli.Command, error) {
-			return &command.OutputCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"plan": func() (cli.Command, error) {
-			return &command.PlanCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"providers": func() (cli.Command, error) {
-			return &command.ProvidersCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"providers lock": func() (cli.Command, error) {
-			return &command.ProvidersLockCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"providers mirror": func() (cli.Command, error) {
-			return &command.ProvidersMirrorCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"providers schema": func() (cli.Command, error) {
-			return &command.ProvidersSchemaCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"push": func() (cli.Command, error) {
-			return &command.PushCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"refresh": func() (cli.Command, error) {
-			return &command.RefreshCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"show": func() (cli.Command, error) {
-			return &command.ShowCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"taint": func() (cli.Command, error) {
-			return &command.TaintCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"test": func() (cli.Command, error) {
-			return &command.TestCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"validate": func() (cli.Command, error) {
-			return &command.ValidateCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"version": func() (cli.Command, error) {
-			return &command.VersionCommand{
-				Meta:              meta,
-				Version:           Version,
-				VersionPrerelease: VersionPrerelease,
-				Platform:          getproviders.CurrentPlatform,
-				CheckFunc:         commandVersionCheck,
-			}, nil
-		},
-
-		"untaint": func() (cli.Command, error) {
-			return &command.UntaintCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"workspace": func() (cli.Command, error) {
-			return &command.WorkspaceCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"workspace list": func() (cli.Command, error) {
-			return &command.WorkspaceListCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"workspace select": func() (cli.Command, error) {
-			return &command.WorkspaceSelectCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"workspace show": func() (cli.Command, error) {
-			return &command.WorkspaceShowCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"workspace new": func() (cli.Command, error) {
-			return &command.WorkspaceNewCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"workspace delete": func() (cli.Command, error) {
-			return &command.WorkspaceDeleteCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		//-----------------------------------------------------------
-		// Plumbing
-		//-----------------------------------------------------------
-
-		"force-unlock": func() (cli.Command, error) {
-			return &command.UnlockCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"state": func() (cli.Command, error) {
-			return &command.StateCommand{}, nil
-		},
-
-		"state list": func() (cli.Command, error) {
-			return &command.StateListCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"state rm": func() (cli.Command, error) {
-			return &command.StateRmCommand{
-				StateMeta: command.StateMeta{
-					Meta: meta,
-				},
-			}, nil
-		},
-
-		"state mv": func() (cli.Command, error) {
-			return &command.StateMvCommand{
-				StateMeta: command.StateMeta{
-					Meta: meta,
-				},
-			}, nil
-		},
-
-		"state pull": func() (cli.Command, error) {
-			return &command.StatePullCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"state push": func() (cli.Command, error) {
-			return &command.StatePushCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"state show": func() (cli.Command, error) {
-			return &command.StateShowCommand{
-				Meta: meta,
-			}, nil
-		},
-
-		"state replace-provider": func() (cli.Command, error) {
-			return &command.StateReplaceProviderCommand{
-				StateMeta: command.StateMeta{
-					Meta: meta,
-				},
-			}, nil
+	commands := map[string]FlatEarthCommand{
+		"flat-earth": &command.FlatEarthGraphCommand{
+			Meta: meta,
 		},
 	}
-
-	PrimaryCommands = []string{
-		"init",
-		"validate",
-		"plan",
-		"apply",
-		"destroy",
-	}
-
-	HiddenCommands = map[string]struct{}{
-		"env":             struct{}{},
-		"internal-plugin": struct{}{},
-		"push":            struct{}{},
-	}
-
+	return commands
 }
 
 // makeShutdownCh creates an interrupt listener and returns a channel.
@@ -421,4 +96,8 @@ func makeShutdownCh() <-chan struct{} {
 func credentialsSource(config *cliconfig.Config) (auth.CredentialsSource, error) {
 	helperPlugins := pluginDiscovery.FindPlugins("credentials", globalPluginDirs())
 	return config.CredentialsSource(helperPlugins)
+}
+
+type FlatEarthCommand interface {
+	Run(configPath string) ([]terraform.GraphTransformer, error)
 }
--- help.go
+++ help.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"log"
-	"sort"
 	"strings"
 
 	"github.com/mitchellh/cli"
@@ -12,56 +11,7 @@ import (
 
 // helpFunc is a cli.HelpFunc that can is used to output the help for Terraform.
 func helpFunc(commands map[string]cli.CommandFactory) string {
-	// Determine the maximum key length, and classify based on type
-	var otherCommands []string
-	maxKeyLen := 0
-
-	for key := range commands {
-		if _, ok := HiddenCommands[key]; ok {
-			// We don't consider hidden commands when deciding the
-			// maximum command length.
-			continue
-		}
-
-		if len(key) > maxKeyLen {
-			maxKeyLen = len(key)
-		}
-
-		isOther := true
-		for _, candidate := range PrimaryCommands {
-			if candidate == key {
-				isOther = false
-				break
-			}
-		}
-		if isOther {
-			otherCommands = append(otherCommands, key)
-		}
-	}
-	sort.Strings(otherCommands)
-
-	// The output produced by this is included in the docs at
-	// website/source/docs/cli/commands/index.html.markdown; if you
-	// change this then consider updating that to match.
-	helpText := fmt.Sprintf(`
-Usage: terraform [global options] <subcommand> [args]
-
-The available commands for execution are listed below.
-The primary workflow commands are given first, followed by
-less common or more advanced commands.
-
-Main commands:
-%s
-All other commands:
-%s
-Global options (use these before the subcommand, if any):
-  -chdir=DIR    Switch to a different working directory before executing the
-                given subcommand.
-  -help         Show this help output, or the help for a specified subcommand.
-  -version      An alias for the "version" subcommand.
-`, listCommands(commands, PrimaryCommands, maxKeyLen), listCommands(commands, otherCommands, maxKeyLen))
-
-	return strings.TrimSpace(helpText)
+	return "Help is disabled since this is a modded version."
 }
 
 // listCommands just lists the commands in the map with the
