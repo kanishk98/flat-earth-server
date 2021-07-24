cc3071f84ac505edbabd8d0725d20e033dec9051
Kanishk Kakar <kanishk.kakar@gmail.com>
2021-07-25 03:45:25 +0530

    Enable block edits with hclwrite.

--- flat_earth_server.go
+++ flat_earth_server.go
@@ -4,20 +4,26 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/zclconf/go-cty/cty"
 )
 
 type FlatEarthGraphUpdateRequest struct {
-	NodeKey       string
+	BlockType     string
+	BlockLabel    string
+	BlockName     string
 	AttributeName string
 	NewValue      string
+	AttributeType string
 }
 
 func check(e error, w http.ResponseWriter) bool {
@@ -53,56 +59,106 @@ func getFlatEarthGraph(w http.ResponseWriter, r *http.Request) {
 	w.Write(graphBytes)
 }
 
-// kanishk98: could we use hclwrite here instead? otherwise we're just using string manipulation
+func getMatchingBlock(folder string, w http.ResponseWriter, blockType string, blockLabels []string) (*hclwrite.Block, string, *hclwrite.File) {
+	var block *hclwrite.Block
+	var fileName string
+	var parsedConfig *hclwrite.File
+	filepath.WalkDir(folder, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatalf("Failed to walk the %s directory", path)
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		} else if filepath.Ext(path) == ".tf" {
+			tfBytes, err := ioutil.ReadFile(path)
+			if check(err, w) {
+				return err
+			}
+			hclConfig, diags := hclwrite.ParseConfig(tfBytes, path, hcl.Pos{Line: 0, Column: 0, Byte: 0})
+			if diags != nil && diags.HasErrors() {
+				log.Fatalf("Error when parsing TF config: %s", diags.Error())
+				// move on
+				return nil
+			}
+			block = hclConfig.Body().FirstMatchingBlock(blockType, blockLabels)
+			if block == nil {
+				return nil
+			}
+			fileName = path
+			parsedConfig = hclConfig
+			// no reason to continue looping over files, we have our block
+			return fmt.Errorf("Fake error to exit loop!")
+		} else {
+			// it's a file we don't care about, move on
+			return nil
+		}
+	})
+	return block, fileName, parsedConfig
+}
+
 func updateFlatEarthGraph(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
-	if check(err, w) { return }
+	if check(err, w) {
+		return
+	}
 	var updateRequest FlatEarthGraphUpdateRequest
 	err = json.Unmarshal(body, &updateRequest)
-	if check(err, w) { return }
-	graph, _ := commands["flat-earth"].Run(args[0])
-	fileName := graph[updateRequest.NodeKey].DeclRange.Filename
-	// kanishk98: Possibilities of the key not getting found here, handle that
-	attrs, _ := graph[updateRequest.NodeKey].Config.JustAttributes()
-	start := attrs[updateRequest.AttributeName].Expr.Range().Start
-	end := attrs[updateRequest.AttributeName].Expr.Range().End
-	tfBytes, err := ioutil.ReadFile(fileName)
-	if check(err, w) { return }
-	startBytes := tfBytes[:start.Byte]
-	endBytes := tfBytes[end.Byte:]
-	newString := string(startBytes) + updateRequest.NewValue + string(endBytes)
-	err = ioutil.WriteFile(fileName, []byte(newString), 0644)
-	if check(err, w) { return }
+	if check(err, w) {
+		return
+	}
+	block, fileName, hclConfig := getMatchingBlock(args[0], w, updateRequest.BlockType, []string{updateRequest.BlockName, updateRequest.BlockLabel})
+	block.Body().SetAttributeValue(updateRequest.AttributeName, cty.StringVal(updateRequest.NewValue))
+	err = ioutil.WriteFile(fileName, hclConfig.Bytes(), 0644)
+	if check(err, w) {
+		return
+	}
 	getFlatEarthGraph(w, r)
 }
 
 func getProviderSchema(w http.ResponseWriter, r *http.Request) {
 	schema, err := commands["provider-schema"].GetProviderSchema(args[0])
-	if check(err, w) { return }
+	if check(err, w) {
+		return
+	}
 	w.Write(schema)
 }
 
 func createNewBlock(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
-	if check(err, w) { return }
+	if check(err, w) {
+		return
+	}
 	var newBlockRequest map[string]interface{}
 	err = json.Unmarshal(body, &newBlockRequest)
 	blockType, err := getKeyValue("blockType", newBlockRequest)
-	if check(err, w) { return }
+	if check(err, w) {
+		return
+	}
 	blockName, err := getKeyValue("blockName", newBlockRequest)
-	if check(err, w) { return }
-	blockLabel, err :=getKeyValue("blockLabel", newBlockRequest)
-	if check(err, w) { return }
-  	file := hclwrite.NewFile()
+	if check(err, w) {
+		return
+	}
+	blockLabel, err := getKeyValue("blockLabel", newBlockRequest)
+	if check(err, w) {
+		return
+	}
+	file := hclwrite.NewFile()
 	fileBody := file.Body()
 	newBlock := fileBody.AppendNewBlock(blockType.(string), []string{blockName.(string), blockLabel.(string)})
 	attributes, err := getKeyValue("attributes", newBlockRequest)
-	if check(err, w) { return }
+	if check(err, w) {
+		return
+	}
 	for attributeKey, attributeValue := range attributes.(map[string]interface{}) {
 		valueType, err := getKeyValue("type", attributeValue.(map[string]interface{}))
-		if check(err, w) { return }
+		if check(err, w) {
+			return
+		}
 		valueValue, err := getKeyValue("value", attributeValue.(map[string]interface{}))
-		if check(err, w) { return }
+		if check(err, w) {
+			return
+		}
 		switch valueType {
 		case "string":
 			newBlock.Body().SetAttributeValue(attributeKey, cty.StringVal(valueValue.(string)))
@@ -125,10 +181,10 @@ func startServer(commands map[string]FlatEarthCommand, args []string) {
 	http.HandleFunc("/create-new-block", createNewBlock)
 	http.HandleFunc("/update-flat-earth-graph", updateFlatEarthGraph)
 	// kanishk98: if this port is in use, the app just crashes with a garbage error.
-	// handle that case early on and either switch to another port (preferably) 
+	// handle that case early on and either switch to another port (preferably)
 	// or present some usable info to the user
 	log.Println("Starting server 💩")
-	idleConnsClosed := make(chan struct {})
+	idleConnsClosed := make(chan struct{})
 	go func() {
 		sigint := make(chan os.Signal, 1)
 		signal.Notify(sigint, os.Interrupt)
